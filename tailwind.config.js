cb865de0f4a4986c47dea564c7d2e4de6688f3dc
Ricardo <82323662+Luziferri@users.noreply.github.com>
2025-08-14 15:50:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['/Users/ricardojanz/Desktop/Red Apple/Coding/bathroom-rank/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
